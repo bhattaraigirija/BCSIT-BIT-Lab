96679a3220d748c344c80ed01be53b4cf671162b
girija <girijaprasadbhattarai322@gmail.com>
2025-06-08 22:47:05 +0545

    after push api

--- app/src/main/java/com/bit/mobileprogramming/MainActivity.kt
+++ app/src/main/java/com/bit/mobileprogramming/MainActivity.kt
@@ -36,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         super.attachBaseContext(context)
     }
 
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
